8f653d7c730ff0befbbc70c216d298bacef42a06
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-09-06 11:48:17 -0500

    Unnecessary Files uploaded to the Repo

--- HeadFilesTutorial/common.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "header.h"
-
-#ifndef HEADFILESTUTORIAL_COMMON_H
-#define HEADFILESTUTORIAL_COMMON_H
-
-#endif //HEADFILESTUTORIAL_COMMON_H
--- HeadFilesTutorial/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "header.h"
-
-int main() {
-    InitLog();
-    Log("Hello World!");
-    return 0;
-}
