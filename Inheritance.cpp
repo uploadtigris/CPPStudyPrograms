873a3cc4290c44d1932d96711ba64d6a5df97fb0
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2021-09-14 21:48:02 -0500

    Create Inheritance.cpp

---
+++ Inheritance.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+//Inheritance;
+//extends an existing class and provide functionality to an existing base class.
+
+class Entity
+{
+public:
+    float X, Y;
+
+    void Move(float xa, float ya)
+    {
+        X += xa;
+        Y += ya;
+    }
+};
+                            //Player is now a sub-class of entity
+                            //Player is now a "super-set" of entity, having all entity has an more.
+class Player : public Entity//Player now has everything that entity has.
+{                           //Player class is now everything entity is plus more.
+public:
+    const char* Name;
+
+    void PrintName()
+    {
+        std::cout << Name << std::endl;
+    }
+};
+
+int main() {
+    std::cout << sizeof(Player) << std::endl;
+
+    Player player;
+    player.Move(5, 5);
+    player.X = 2;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
